368d62f8ee003848220c8f1528c5c2f22ee40877
scal9000 <scaletta@cs.tu-darmstadt.de>
2026-06-20 20:19:12 +0200

    ++

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ sudo  apt-get install < dependecies
 cp .key4cats_command.sh ~/.key4cats_command.sh
 cd ..
 git clone https://github.com/MarcoScaletta/key4cats.git
-cd key4cats/
 cd key4cats/ 
 echo "export KEY4CATs=$(pwd)/cats/build/libs/KeY4CATs.jar" > ~/.bash_profile
 echo "source ~/.key4cats_command.sh" >> ~/.bash_profile
