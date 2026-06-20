9e32d9bb0b6ba328983ff480bf9f6877b1477b56
scal9000 <scaletta@cs.tu-darmstadt.de>
2026-06-20 20:17:29 +0200

    ++

--- setup.sh
+++ setup.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-sudo su
-apt-get update && apt-get upgrade -y && apt-get autoremove -y
+sudo apt-get update
+sudo apt-get upgrade -y 
+sudo apt-get autoremove -y
 
-apt-get install git
-apt-get install openjdk-21-jdk
-apt-get install gradle
-
-git clone https://github.com/MarcoScaletta/key4cats.git
-
-cd key4cats/
-
-echo "KEY4CATs=$(pwd)/cats/build/libs/KeY4CATs.jar" >> ~/.bashrc
-echo "source ~/.key4cats_command.sh" >> ~./bashrc
+sudo  apt-get install < dependecies
 
 cp .key4cats_command.sh ~/.key4cats_command.sh
-
-source ~/.bashrc
+cd ..
+git clone https://github.com/MarcoScaletta/key4cats.git
+cd key4cats/
+cd key4cats/ 
+echo "export KEY4CATs=$(pwd)/cats/build/libs/KeY4CATs.jar" > ~/.bash_profile
+echo "source ~/.key4cats_command.sh" >> ~/.bash_profile
+source ~/.bash_profile
 echo $KEY4CATs
-
-./gradlew :cats:shadowjar
+/gradlew :cats:shadowjar
